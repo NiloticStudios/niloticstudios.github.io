3ed51316998b2057f3a3629876e9778078bbf594
young <youngmwanzia@gmail.com>
2024-04-28 19:26:21 +0300

    Mailchimp integration

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="flex items-center">
+    <form class="flex items-center"  action="https://gmail.us18.list-manage.com/subscribe/post?u=3e3ec6deb39f99b4a249ac797&amp;id=73764c67e2&amp;f_id=009764e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
     <input type="email" placeholder="Enter your email" class="p-2 border border-gray-300 rounded-l-md focus:outline-none focus:border-gray-500" />
     <button type="submit" class="p-2 bg-gray-500 text-white rounded-r-md hover:bg-gray-600">Subscribe</button>
     </form>
