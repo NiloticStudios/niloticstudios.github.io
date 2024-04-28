445880669a60fd79fe52b791ce09bf53eacab2c9
young <youngmwanzia@gmail.com>
2024-04-29 00:52:18 +0300

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  app: {
+    baseURL: '/niloticstudios.github.io/',
+    buildAssetsDir: 'assets'
+  },
+
   vite: {
     css: {
       preprocessorOptions: {
